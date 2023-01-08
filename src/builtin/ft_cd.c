0b1d497f3c668dac6cb75a558a78e10bc38a3282
minsukim <minsu980107@naver.com>
2023-01-08 23:59:23 +0900

    cd 홈디렉터리 구현중

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -19,6 +19,10 @@ void	ft_cd(t_tree_cmd *cmd, t_detower *env_tower)
 	pwd_path = ft_getcwd(NULL, 0);
 	if (pwd_path == NULL)
 		return ;
+	if (cmd->cmd_argv[1] == "~")
+	{// 홈 디렉터리
+
+	}
 	if (cmd->cmd_argv[1] != NULL)
 	{
 		if (ft_chdir(cmd->cmd_argv[1]) == -1)
@@ -31,3 +35,27 @@ void	ft_cd(t_tree_cmd *cmd, t_detower *env_tower)
 	change_oldpwd(env_tower, pwd_path);
 	free(pwd_path);
 }
+
+static void	chdir_home(t_detower *env_tower)
+{
+	t_d_list		*env;
+	t_env_ctnt		*set_pwd;
+	char			*pwd_path;
+
+	pwd_path = ft_getcwd(NULL, 0);
+	if (pwd_path == NULL)
+		return ;
+	env = env_tower->head;
+	set_pwd = find_env_keycmp(env, "HOME");
+	if (set_pwd != NULL)
+	{
+		free(set_pwd->key);
+		if (set_pwd->value != NULL)
+			free(set_pwd->value);
+		set_env_value(set_pwd, "HOME", pwd_path);
+	}
+	free(pwd_path);
+}
+
+
+
