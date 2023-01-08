73c9581770beca872a3d79df8bc5033317ab84ee
minsukim <minsu980107@naver.com>
2023-01-09 00:20:18 +0900

    cd home 구현 완료

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -20,9 +20,11 @@ void	ft_cd(t_tree_cmd *cmd, t_detower *env_tower)
 	if (pwd_path == NULL)
 		return ;
 	if (cmd->cmd_argv[1] == "~")
-	{// 홈 디렉터리
-
-	}
+		if (ft_chdir(ft_getenv("HOME")) == -1)
+		{
+			free(pwd_path);
+			return ;
+		}
 	if (cmd->cmd_argv[1] != NULL)
 	{
 		if (ft_chdir(cmd->cmd_argv[1]) == -1)
@@ -35,27 +37,3 @@ void	ft_cd(t_tree_cmd *cmd, t_detower *env_tower)
 	change_oldpwd(env_tower, pwd_path);
 	free(pwd_path);
 }
-
-static void	chdir_home(t_detower *env_tower)
-{
-	t_d_list		*env;
-	t_env_ctnt		*set_pwd;
-	char			*pwd_path;
-
-	pwd_path = ft_getcwd(NULL, 0);
-	if (pwd_path == NULL)
-		return ;
-	env = env_tower->head;
-	set_pwd = find_env_keycmp(env, "HOME");
-	if (set_pwd != NULL)
-	{
-		free(set_pwd->key);
-		if (set_pwd->value != NULL)
-			free(set_pwd->value);
-		set_env_value(set_pwd, "HOME", pwd_path);
-	}
-	free(pwd_path);
-}
-
-
-
